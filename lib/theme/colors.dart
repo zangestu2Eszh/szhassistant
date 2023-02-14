ed7a270f534f40de461a7ea41a2cd3ac675421b0
Zangestu <59559955+zangestu2Eszh@users.noreply.github.com>
2023-02-14 15:37:00 +0300

    first v

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,7 +1,6 @@
-
 import 'package:flutter/material.dart';
 
-Color black=Colors.black;
-Color white=Colors.white;
-Color gray=Colors.grey;
-Color emptyColor=Colors.transparent;
+Color black = Colors.black;
+Color white = Colors.white;
+Color gray = Colors.grey;
+Color emptyColor = Colors.transparent;
